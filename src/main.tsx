722bdd933e9817f40a54e93ed0a75a1880a4419f
saymamutovsaidakmol@gmail.com <116891087+saidkamol2006@users.noreply.github.com>
2025-06-16 19:16:42 +0500

    i18m

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "./i18n"
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import "./i18n"
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
